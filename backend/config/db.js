5722209809132a49172bcf0290c866b332c24b88
Rei Ikonomi <rei.ikonomi@goteacher.com>
2024-09-24 21:27:31 +0200

    fixed connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://estel:dbEstelDona@atlascluster.o2wxhlq.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster')
+    await mongoose.connect(process.env.MONGO_URI);
 
-    console.log('MongoDB connected...');
+    console.log("MongoDB connected...");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
@@ -12,69 +12,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-
-
-
-
-//mongodb+srv://edona:mXnQg9044yPgipCs@cluster0.caqq1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
-
-
-//mXnQg9044yPgipCs
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
